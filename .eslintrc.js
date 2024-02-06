18fc1b19498ca60610a2f5b48ce4527ad351d2cd
Sarthak Aggarwal <sarthak.aggarwal@students.iiit.ac.in>
2024-02-06 17:54:20 +0530

    changes in lint files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,27 @@ module.exports = {
     ecmaVersion: 2018,
   },
   parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended"],
-  plugins: ["remedo-lint"],
+  extends: ["eslint:recommended","plugin:jsx-a11y/recommended","plugin:@typescript-eslint/recommended","plugin:eslint-comments/recommended","plugin:tailwindcss/recommended"],
+  plugins: ["remedo-lint","@typescript-eslint","jsx-a11y","tailwindcss","validate-jsx-nesting","eslint-comments"],
   rules: {
-    "remedo-lint/require-id": [1, { include: [], exclude: [] }],
-    "remedo-lint/no-console": [1, { include: [], exclude: [] }],
-    "remedo-lint/no-inline-style-literal": [1, { include: [], exclude: [] }],
-    "remedo-lint/no-array-indices-in-carray": [1, { include: [], exclude: [] }],
+    "remedo-lint/require-id": [2, { include: [], exclude: [] }],
+    "remedo-lint/no-console": [2, { include: [], exclude: [] }],
+    "remedo-lint/no-inline-style-literal": [2, { include: [], exclude: [] }],
+    "remedo-lint/no-array-indices-in-carray": [2, { include: [], exclude: [] }],
+    "remedo-lint/parent-div-id": [2, { include: [], exclude: [] }],
+    "remedo-lint/style-classname-props": [2, { include: [], exclude: [] }],
+    "validate-jsx-nesting/no-invalid-jsx-nesting": "error",
+    "remedo-lint/optional-props": [2, { include: [], exclude: [] }],
   },
+  overrides: [
+    {
+      files: ["src/**/*.tsx"],
+      rules:{
+        "tailwindcss/classnames-order": "off",
+        "tailwindcss/no-custom-classname": "off",
+        "tailwindcss/no-arbitrary-value":"error",
+        "eslint-comments/require-description": ["error", {"ignore": []}]
+      }
+    },
+  ],
 };
